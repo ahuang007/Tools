8cda7e3821c53ecd7cf1937b48feb89277018fbe
ahuang007 <419637330@qq.com>
2016-04-06 17:53:27 +0800

    修改checkError.py

--- checkError.py
+++ checkError.py
@@ -6,26 +6,46 @@ import  smtplib
 from    email.mime.text import MIMEText
 from    email.header import Header
 import  re
-import  socket
+import  time
+import thread
 
-#发送对象
-mailto_list=[
-    "andy@61games.hk",
+#配置
+ip_addr     = "10.10.2.58"          #ip地址 或者 服务器名称
+ProjectName = "美味战争"            #项目名
+logdir      = os.getcwd() + "/log"  #路径
+interval    = 5#60 * 60               #执行间隔时间
+
+#邮件列表(接收者)
+mailto_list = [
+    "andy@61games.hk",      #andy
 ]
 
+#发送者
 mail_host       = "smtp.qq.com"
 mail_user       = "419637330"
 mail_pass       = "wqnecpxkaiobbjha" #ssl认证
 mail_postfix    = "qq.com"
 
-def send_mail(to_list,title,content):
-    '''
-    to_list :邮件地址列表
-    title   :标题
-    content :内容
-    send_mail("xxx@qq.com","title","content")
-    '''
-    me = "%s<"%(Header('《美味战争》服务器警告邮件','utf-8')) + mail_user + "@" + mail_postfix+">"
+#文件当前读取记录 发送后要重置 隔天要重置
+line_tmp  = {
+    'gamed'             : 0, #gamed-20160406.log
+    'center'            : 0,
+    'gslb'              : 0,
+    'login'             : 0,
+    'comrank'           : 0,
+    'logserver'         : 0,
+    'rank'              : 0,
+    'callbacklogger'    : 0, #callbacklogger.20160406-1.log
+}
+
+'''
+to_list :邮件地址列表
+title   :标题
+content :内容
+send_mail("xxx@qq.com","title","content")
+'''
+def send_mail(to_list, title, content):
+    me = "%s<"%(Header('《' + ProjectName + '》服务器警告邮件','utf-8')) + mail_user + "@" + mail_postfix+">"
     msg = MIMEText(content, format, 'utf-8')
     msg['Subject'] = title
     msg['From'] = me
@@ -43,45 +63,67 @@ def send_mail(to_list,title,content):
     except Exception, e:
         print str(e)
         return False
+    thread.exit_thread() #发送邮件结束后线程退出
 
-#ip地址
-ip          = "10.10.2.58"
-#项目名
-ProjectName = "美味战争"
-#路径
-logdir      = os.getcwd() + "/log"
 
-is_send = False
+##定时检测 【正常情况下：一个进程一天生成一个文件】
+def check_Error(now):
+    date = time.strftime('%Y%m%d', now)
+    print("exec check_Error " +  time.strftime('%Y%m%d %H:%M:%S', now))
+    print line_tmp
 
-##定时检测
-for parent,dirnames,filenames in os.walk(logdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-    for filename in filenames:                       #输出文件信息
-        print "filename :" + filename
+    for k in line_tmp:
+        process_name = k
+        last_file_line = line_tmp[k]
+        log_file_name = ""
+        if process_name != 'callbacklogger':
+            log_file_name = process_name + "-" + date + ".log" # 默认生成一个文件 后缀不带-n
+        else:
+            log_file_name = process_name + "." + date + "-1.log" # 默认生成一个后缀-1的文件
 
-        #读取文件
-        file_object = open(logdir + "/" + filename)
+        is_exist_file = os.path.exists(logdir + "/" + log_file_name)
 
-        last_line = ""
-        try:
+        if is_exist_file:
+            print log_file_name + " is exist"
+            line_list = [] #发送一个小时内的邮件
+            last_line = ""
+
+            file_object = open(logdir + "/" + log_file_name)
+            line_it = 0
             for line in file_object:
-                if re.match("stack traceback", line):
-                    title       = filename
-                    line_list   = [last_line, line]
-                    next_line   = file_object.next()
-                    while not re.match("^[", next_line):
-                        line_list.append(next_line)
-                        next_line = file_object.next()
-
-                    content     = "\n".join(line_list)
-
-                    if not is_send:
-                        is_send = True
-                        send_mail(mailto_list, title, content)
-
-                last_line = line
-        finally:
+                line_it += 1
+                if line_it > last_file_line:
+                    line_tmp[k] += + 1
+                    if re.match("stack traceback", line):
+                        line_list.extend(["\n", last_line])
+                    if not re.match("\[", line):
+                        line_list.append(line)
+            last_line = line
             file_object.close()
+        else:
+            print log_file_name + " is not exist"
 
-        #判断当天的
-        #判断是否重复
-        #发送邮件
+        if len(line_list):
+            title       = ip_addr + " || " + log_file_name
+            content     = "\n".join(line_list)
+            thread.start_new_thread(send_mail, (mailto_list, title, content))
+
+last_check_time = time.localtime()
+def is_same_day(beginDate,endDate):
+    format="%Y-%m-%d";
+    bd = time.strftime(format, beginDate)
+    ed = time.strftime(format, endDate)
+    if bd == ed:
+        return True
+    else:
+        return False
+
+while True:
+    now  = time.localtime()
+    #隔天处理
+    if not is_same_day(last_check_time, now):
+        for k in line_tmp:
+            line_tmp[k] = 0
+        last_check_time = now
+    check_Error(now)
+    time.sleep(interval)
